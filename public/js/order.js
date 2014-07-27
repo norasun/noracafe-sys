e58c14930295d29b0ffaee08f1fe9ac5aa7e2b2e
norasun <norasun@norasuntekiMacBook-Pro.local>
2014-07-27 22:58:27 +0800

    完成了订单页react的初步整合

--- public/js/order.js
+++ public/js/order.js
@@ -3,25 +3,32 @@
 
   //产品
   var Product = React.createClass({
+    onClick: function(){
+      this.props.productClick({productId: this.props.productId, name: this.props.name});
+
+    },
     render: function(){
       return (
-        <a className="Product" onClick={this.onClick} href="#" >
-          {this.props.name}
-          ¥{this.props.price}
+        <a className="Product clearfix" onClick={this.onClick} href="#" >
+          <span className="pull-left">
+            {this.props.name}
+          </span>
+          <span className="pull-right">
+            ¥{this.props.price}
+          </span>
+
         </a>
       );
-    },
-    onClick: function(){
-      orderdata = orderdata2;
     }
   });
 
   //产品列表
   var ProductList = React.createClass({
     render: function(){
+      var obj = this;
       var productNodes = this.props.data.map(function(product){
         return (
-          <Product name={product.name} price={product.price} />
+          <Product name={product.name} price={product.price} productId={product.id} productClick={obj.props.productClick} />
         );
 
 
@@ -41,7 +48,7 @@
     render: function(){
       return (
         <div className="productBox">
-          <ProductList data={this.props.data} />
+          <ProductList data={this.props.data} productClick={this.props.productClick} />
         </div>
       );
     },
@@ -70,6 +77,7 @@
       return (
         <div className="Order">
           {this.props.name}
+          {this.props.num}份
         </div>
       );
     }
@@ -80,7 +88,7 @@
     render: function(){
       var orderNodes = this.props.data.map(function(order){
         return (
-          <Order name={order.name} />
+          <Order name={order.name} num={order.num} />
         );
       });
       return (
@@ -93,9 +101,7 @@
 
   //订单控件
   var OrderBox = React.createClass({
-    getInitialState: function() {
-      return {data: []};
-    },
+
     render: function(){
       return (
           <div class="OrderBox">
@@ -106,22 +112,82 @@
     }
   });
 
+//订单预处理
+  var PreOrder = React.createClass({
+    getInitialState: function() {
+
+      return {productId: '', productName: '', productNum: 1};
+    },
+    onClick: function(){
+
+      this.props.orderSubmit({productName: this.props.name, productNum: this.refs.productNum.getDOMNode().value});
+    },
+    render: function(){
+      if(this.props.show == 'show'){
+
+        return (
+
+          <div className="PreOrder">
+            {this.props.name}
+
+            <select onChange={this.onChange} ref="productNum">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <button type="button" onClick={this.onClick} className="btn btn-mwm">添加</button>
+          </div>
+        );
+      }else{
+        return (
+          null
+        );
+      }
+
+    }
+  });
+
   //订单app
   var OrderApp = React.createClass({
     getInitialState: function() {
-      return {data: data, orderData: orderdata};
+
+      return {data: data, orderData: orderdata, preOrder:[]};
+    },
+    findProduct: function(obj){
+      //从数据中找到相应的产品数据
+      var wow = this;
+
+      $.each(this.state.data, function(i, v){
+
+        if(obj.target.id == v.id){
+
+          wow.setState({data:data, orderData:[{name:'xx'}], preOrder: {show: 'show', productId: v.id, productName: v.name}});
+
+        }
+
+      });
     },
     onClick: function(e){
-      this.setState({data: [{name:'好呀！'}], orderData:[{name:'xx'}]});
-      alert(e.target.name);
+      //alert($(this).atrr('name'));
+      //this.setState({data: data, orderData:[{name:'xx'}]});
+      this.findProduct(e);
+    },
+    productClick: function(productInfo){
+
+      this.setState({data:data, orderData:[{name:'xx'}], preOrder: {show: 'show', productId: productInfo.productId, productName: productInfo.name}});
+    },
+    orderSubmit: function(orderInfo){
+      this.setState({data:data, orderData:[{name: orderInfo.productName, num: orderInfo.productNum}], preOrder: {show: '', productId: '', productName: ''}});
+
     },
     render: function(){
       var obj = this;
       var productNodes = this.state.data.map(function(product){
         return (
-          <a className="Product" onClick={obj.onClick} href="#" name={product.name} >
-            {product.name}
-            ¥{product.price}
+          <a className="Product" onClick={obj.onClick} href="#" id={product.id} >
+            {product.name} ¥{product.price}
           </a>
         );
       });
@@ -129,14 +195,12 @@
       return (
         <div className="row">
           <div className="col-md-8">
-            <div className="white-container" >
-              <div className="productBox">
-                <div className="productList">
-                  {productNodes}
-                </div>
-              </div>
+            <div className="white-container">
+              <ProductBox data={this.state.data} productClick={this.productClick} />
+              <PreOrder data={this.state.preOrder} show={this.state.preOrder.show} name={this.state.preOrder.productName} orderSubmit={this.orderSubmit}/>
             </div>
           </div>
+
           <div className="col-md-4">
             <div className="black-container">
               订单
